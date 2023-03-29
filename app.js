55089378d533e1c353290a17c461de78e851e6c8
ERose18 <evie.nulty@gmail.com>
2023-03-29 11:00:08 +0100

    Now gives you comments when given a review_id, Completing pt.6

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const {getCategories} = require('./controllers/get-categories-controllers');
 const {getReviewsByID} = require('./controllers/get-reviews-id-controller');
 const { getReviews } = require('./controllers/get-reviews-controllers');
+const {getReviewCommentsByID} = require('./controllers/get-reviews-comments-controllers');
 const express = require('express');
 const { handlePSQL400s, handleCustomError, handle500 } = require('./error-handling');
 
@@ -11,6 +12,8 @@ app.get('/api/categories', getCategories);
 app.get('/api/reviews/:review_id', getReviewsByID);
 
 app.get('/api/reviews', getReviews);
+
+app.get('/api/reviews/:review_id/comments', getReviewCommentsByID);
 
 app.all('/*', (req, res) => {
     res.status(404).send({msg: 'End-point Not Found'})
